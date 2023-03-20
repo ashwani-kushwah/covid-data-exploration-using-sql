93341df5a150af7d65fe600d2f11f1b1fe0ddfc3
Ashwani <ashwanikushwah72@gmail.com>
2023-03-20 18:31:22 +0530

    second commit

--- covid data sql project.sql
+++ covid data sql project.sql
@@ -1,6 +1,20 @@
+/*
+Covid 19 Data Exploration 
+
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+
+*/
+
+
+
+
 select * from sqlproject..covidDeaths
 where continent is not null
 order by 3,4
+
+
+
+-- Select Data that we are going to be starting with
 
 select location, date, total_cases, new_cases, total_deaths, population
 from sqlproject..covidDeaths
@@ -8,7 +22,10 @@ where continent is not null
 order by 1,2
 
 
---total cases vs total deaths
+
+-- Total cases vs Total deaths
+-- Shows likelihood of dying if you contract covid in your country
+
 select location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as DeathPercentage
 from sqlproject..covidDeaths
 where location='india' and continent is not null
@@ -17,8 +34,8 @@ order by 1,2
 
 
 
---total cases vs population
---shows what percentage of population got covid
+-- Total Cases vs Population
+-- Shows what percentage of population got covid
 select location, date, population, total_cases, (total_cases/population)*100 as CasesPercentage
 from sqlproject..covidDeaths
 where location='india' and continent is not null
@@ -26,7 +43,8 @@ order by 1,2
 
 
 
--- looking at Countries with highest Infection rate compared to population
+
+-- Countries with Highest Infection Rate compared to Population
 
 select location, population, max(total_cases) as HighestCaseCount, max((total_cases/population))*100 as InfectPercentage
 from sqlproject..covidDeaths
@@ -36,7 +54,7 @@ order by InfectPercentage desc
 
 
 
--- looking at Countries with highest Death
+-- Countries with Highest Death Count
 
 select location, max(cast(total_deaths as int)) as TotalDeathCount
 from sqlproject..covidDeaths
@@ -46,13 +64,17 @@ order by TotalDeathCount desc
 
 
 
--- looking at Continent with highest Death
+-- BREAKING THINGS DOWN BY CONTINENT
+
+-- Continent with Highest Death Count By Continent
 
 select continent, max(cast(total_deaths as int)) as TotalDeathCount
 from sqlproject..covidDeaths
 where continent is not null
 group by continent
 order by TotalDeathCount desc
+
+
 
 
 -- Global numbers
@@ -66,7 +88,9 @@ order by 1,2
 
 
 
--- Total population vs total vaccination
+
+-- Total Population vs Vaccinations
+-- Shows Percentage of Population that has recieved at least one Covid Vaccine
 
 select cd.continent, cd.location, cd.date, cd.population, cv.new_vaccinations,
 	sum(cast(cv.new_vaccinations as bigint)) over (partition by cd.location order by cd.location, cd.date) as RollingPeopleVaccinated
@@ -77,7 +101,9 @@ join sqlproject..covidVaccinations cv
 where cd.continent is not null
 order by 2, 3
 
--- using CTE
+
+-- Using CTE to perform Calculation on Partition By in previous query
+
 with PopVsVac(continent, location, date, population, new_vaccinations, RollingPeopleVaccinated)
 as
 (
@@ -98,11 +124,7 @@ order by 2, 3
 
 
 
-
-
-
-
--- using temp table
+-- Using Temp Table to perform Calculation on Partition By in previous query
 
 drop table if exists #PopulationVaccinated
 create table #PopulationVaccinated
@@ -132,7 +154,8 @@ order by 2, 3
 
 
 
--- Creating views
+-- Creating View to store data for later visualizations
+
 create view PopulationVaccinated as
 select cd.continent, cd.location, cd.date, cd.population, cv.new_vaccinations,
 	sum(cast(cv.new_vaccinations as bigint)) over (partition by cd.location order by cd.location, cd.date) as RollingPeopleVaccinated
@@ -141,5 +164,3 @@ join sqlproject..covidVaccinations cv
 	on cd.location = cv.location
 	and cd.date = cv.date
 where cd.continent is not null
-
-select * from PopulationVaccinated
